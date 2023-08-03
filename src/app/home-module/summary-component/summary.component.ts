4b7064ae1a1013025cb116bf57c419a279b0836f
unknown <dimulescualexandru02@gmail.com>
2023-08-03 14:10:24 +0300

    Displayed data on the SummaryComponent without Total and TotalCalories

--- src/app/home-module/summary-component/summary.component.ts
+++ src/app/home-module/summary-component/summary.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SchedulerService } from 'src/app/shared-module/scheduler-service/scheduler.service';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.css']
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnDestroy {
+
+summaryDays:any;
+days = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri'];
+objectKeys = Object.keys;
+subscription!: Subscription;
+constructor(private DailyService: SchedulerService) {
+  this.subscription = this.DailyService.schedule.subscribe((res) => {
+    this.summaryDays = res;
+    console.log(this.summaryDays)
+  })
+}
+
+ngOnDestroy(): void {
+  this.subscription.unsubscribe();
+}
+
 
 }
