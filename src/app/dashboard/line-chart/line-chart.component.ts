1bdd65d7ec58322459daa6c03ba18e980f91f999
CTAndrei <constantin.andrei411@gmail.com>
2023-08-04 14:45:14 +0300

    final charts

--- src/app/dashboard/line-chart/line-chart.component.ts
+++ src/app/dashboard/line-chart/line-chart.component.ts
@@ -9,44 +9,12 @@ import { ChartConfiguration, ChartOptions } from 'chart.js';
 export class LineChartComponent implements OnChanges {
 
   @Input() caloriiPerOra: number[] = [];
-  public lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: [
-      '00:00',
-      '01:00',
-      '02:00',
-      '03:00',
-      '04:00',
-      '05:00',
-      '06:00',
-      '07:00',
-      '08:00',
-      '09:00',
-      '10:00',
-      '11:00',
-      '12:00',
-      '13:00',
-      '14:00',
-      '15:00',
-      '16:00',
-      '17:00',
-      '18:00',
-      '19:00',
-      '20:00',
-      '21:00',
-      '22:00',
-      '23:00',
-    ],
-    datasets: [
-      {
-        data: [],
-        label: 'KCal',
-        fill: true,
-        tension: 0.5,
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)'
-      }
-    ]
-  };
+
+  public lineChartLabels: string[] = [];
+  public lineChartDatasets: ChartConfiguration<'line'>['data']['datasets'] = [
+    { data: [], label: '' },
+  ];
+
   public lineChartOptions: ChartOptions<'line'> = {
     responsive: false
   };
@@ -56,9 +24,18 @@ export class LineChartComponent implements OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if (changes['caloriiPerOra'] && this.caloriiPerOra) {
-      this.lineChartData.datasets[0]['data'] = this.caloriiPerOra;
-    }
+    this.lineChartDatasets[0].data = [];
+    this.lineChartLabels = [];
 
+    const oreZi = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+    this.lineChartDatasets[0].label = 'KCal';
+    for (const ora of this.caloriiPerOra) {
+      this.lineChartLabels = oreZi;
+      this.lineChartDatasets[0].data.push(ora);
+      this.lineChartDatasets[0].fill = true;
+      this.lineChartDatasets[0].tension = 0.5;
+      this.lineChartDatasets[0].borderColor = 'black';
+      this.lineChartDatasets[0].backgroundColor = 'rgba(255,0,0,0.3)';
+    }
   }
 }
