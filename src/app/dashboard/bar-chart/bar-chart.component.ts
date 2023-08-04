db1836c3857597da533436bafde91b64bb254ee3
CTAndrei <constantin.andrei411@gmail.com>
2023-08-04 14:18:47 +0300

    Update bar-chart in real time

--- src/app/dashboard/bar-chart/bar-chart.component.ts
+++ src/app/dashboard/bar-chart/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
 
 @Component({
@@ -6,49 +6,41 @@ import { ChartConfiguration } from 'chart.js';
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
-export class BarChartComponent implements OnChanges{
+export class BarChartComponent implements OnChanges {
 
   @Input() caloriiPerZi: any[] = [];
   @Input() typeChart: 'kcal' | 'kcalDimS' = 'kcal';
 
-  public barChartLegend = true;
-  public barChartPlugins = [];
-
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['Luni','Marti','Miercuri','Joi','Vineri'],
-    datasets: []
-  };
-
+  public barChartLabels: string[] = [];
+  public barChartDatasets: ChartConfiguration<'bar'>['data']['datasets'] = [
+    { data: [], label: '' },
+  ];
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
   };
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
+    this.barChartLabels = [];
+    this.barChartDatasets[0].data = [];
+
+    const zileSaptamana = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri'];
+
     if (this.typeChart === 'kcal') {
-      this.barChartData.datasets.push({
-        data: this.caloriiPerZi.map(ziuaData => ziuaData.morningCal + ziuaData.eveningCal),
-        label: 'KCal Arse pe Zi',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)'
-      });
+      this.barChartDatasets[0].label = 'KCal Arse pe Zi';
+      for (const activitate of this.caloriiPerZi) {
+        this.barChartLabels.push(zileSaptamana[activitate.day]);
+        this.barChartDatasets[0].data.push(activitate.morningCal + activitate.eveningCal);
+      }
     } else if (this.typeChart === 'kcalDimS') {
-      this.barChartData.datasets.push({
-        data: this.caloriiPerZi.map(ziuaData => ziuaData.morningCal),
-        label: 'KCal Dimineata',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)'
-      });
-
-      this.barChartData.datasets.push({
-        data: this.caloriiPerZi.map(ziuaData => ziuaData.eveningCal),
-        label: 'KCal Seara',
-        backgroundColor: 'rgba(54, 162, 235, 0.5)'
-      });
+      this.barChartDatasets[0].label = 'KCal Dimineata';
+      this.barChartDatasets[1] = { data: [], label: 'KCal Seara' };
+      for (const activitate of this.caloriiPerZi) {
+        this.barChartLabels.push(zileSaptamana[activitate.day]);
+        this.barChartDatasets[0].data.push(activitate.morningCal);
+        this.barChartDatasets[1].data.push(activitate.eveningCal);
+      }
     }
-//     console.log('Datasets:', this.barChartData.datasets);
-//     console.log('date:', this.caloriiPerZi);
-
-    this.barChartData.datasets[0].label = this.typeChart === 'kcal' ? 'KCal Arse pe Zi' : 'KCal Dimineata';
   }
 }
