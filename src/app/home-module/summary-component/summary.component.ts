28cbb7c45a40268f99b2f47ad5e2c5ff8728b351
PopescuStefan1 <stefan.ro95@gmail.com>
2023-08-04 12:16:11 +0300

    Update summary when refreshing page

--- src/app/home-module/summary-component/summary.component.ts
+++ src/app/home-module/summary-component/summary.component.ts
@@ -1,43 +1,46 @@
-import { Component,OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { SchedulerService } from 'src/app/shared-module/scheduler-service/scheduler.service';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.css']
+  styleUrls: ['./summary.component.css'],
 })
-export class SummaryComponent implements OnDestroy {
+export class SummaryComponent implements OnInit, OnDestroy {
+  summaryDays: any;
+  days = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri'];
+  objectKeys = Object.keys;
+  subscription!: Subscription;
+  totalTimeInHours = 0;
+  totalCalories = 0;
 
-summaryDays:any;
-days = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri'];
-objectKeys = Object.keys;
-subscription!: Subscription;
-totalTimeInHours = 0;
-totalCalories= 0;
-constructor(private DailyService: SchedulerService) {
-  this.subscription = this.DailyService.schedule.subscribe((res) => {
-    this.summaryDays = res;
-    console.log(this.summaryDays)
-  })
-}
+  constructor(private DailyService: SchedulerService) {}
 
-ngOnDestroy(): void {
-  this.subscription.unsubscribe();
-}
+  ngOnInit(): void {
+    this.subscription = this.DailyService.schedule.subscribe((res) => {
+      this.summaryDays = res;
+      console.log(this.summaryDays);
+    });
 
-calculateTotalsHours() {
-  const totals = this.DailyService.getTotals();
-  console.log(totals.calories)
-  this.totalTimeInHours = totals.time / 60; 
-  return this.totalTimeInHours;
-}
+    this.summaryDays = this.DailyService.getFullSchedule();
+  }
 
-calculateTotalCalories(){
-  const totals = this.DailyService.getTotals();
-  console.log(totals.calories)
-  this.totalCalories = totals.calories;
-  return this.totalCalories;
-}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
+  calculateTotalsHours() {
+    const totals = this.DailyService.getTotals();
+    console.log(totals.calories);
+    this.totalTimeInHours = totals.time / 60;
+    return this.totalTimeInHours;
+  }
+
+  calculateTotalCalories() {
+    const totals = this.DailyService.getTotals();
+    console.log(totals.calories);
+    this.totalCalories = totals.calories;
+    return this.totalCalories;
+  }
 }
